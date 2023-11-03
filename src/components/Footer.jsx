1f76b2ac3f6b12108413e1c2fba2e267f7b83a26
ankit1519 <raazankit1502@gmail.com>
2023-11-03 09:13:04 +0530

    footer component done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import './Footer.css';
-import { Link } from 'react-router-dom';
-
-const Footer = () => {
- 
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faYoutube, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import './Footer.css'
+function Footer() {
   return (
-    <footer className="footer ">
+    <footer className="footer">
       <div className="container">
-        <div className="social-icons">
-         
-        
+        <div className="footer-content">
+          <div className="footer-links">
+            <a href="https://github.com/ankit1519" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} /> GitHub
+            </a>
+            <a href="https://www.youtube.com/@ankitrajsrivastava" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faYoutube} /> YouTube
+            </a>
+            <a href="https://www.linkedin.com/in/ankitraj-srivastava/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+            </a>
+            <a href="https://twitter.com/this_ankitraj" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitter} /> Twitter
+            </a>
+          </div>
+          <p>&copy; {new Date().getFullYear()} Ankit Raj</p>
         </div>
-        <p className='hi'>&copy; 2023 Your Website Name</p>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
